2795ce3ca5653f0050baf6de2a90a64e0c557089
siegrest <179900@ttu.ee>
2018-03-18 16:28:09 +0200

    Remove google analytics

--- index.php
+++ index.php
@@ -8,15 +8,6 @@
   <link rel="stylesheet" href="assets/css/main.css">
   <script src="assets/js/jquery.min.js"></script>
   <script src="assets/js/bootstrap.min.js"></script>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115952624-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-115952624-1');
-  </script>
 </head>
 <body>
 
