76c6083cefa9051c3fe9450232eb40cb6e89c8b2
siegrest <179900@ttu.ee>
2018-03-18 13:40:28 +0200

    Improve "load more" button functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 // Define "global" variable (ie the largest scope here)
 let SELECTED;
-
+var ITEMS = [];
 const initialLoadAmount = 50;
+const PRICE_PERCISION = 100;
 
 // Load conversion rates on page load
 $(document).ready(function() {
@@ -16,15 +17,17 @@ $(document).ready(function() {
     };
 
     // Define event listener
-    selectorLeague.on("change", function(){
+    selectorLeague.change(function(){
         SELECTED.league = $(this).find(":selected").text();
-        $("#searchResults tbody tr").empty();
+        ITEMS = [];
+        $("#searchResults > tbody").empty();
         makeRequest(0, initialLoadAmount);
     });
     // Define event listener
-    selectorSub.on("change", function(){
+    selectorSub.change(function(){
         SELECTED.sub = $(this).find(":selected").text();
-        $("#searchResults tbody tr").empty();
+        ITEMS = [];
+        $("#searchResults > tbody").empty();
         makeRequest(0, initialLoadAmount);
     });
     // Define event listener
@@ -40,64 +43,78 @@ function makeRequest(from, to) {
     var request = $.ajax({
         url: "http://api.poe.ovh/get",
         data: {
-            async: true,
-            league: $("#search-league").find(":selected").text(), 
-            category: getUrlParameter("category"),
-            sub: $("#search-sub").find(":selected").text(),
+            league: SELECTED.league, 
+            category: SELECTED.category,
+            sub: SELECTED.sub,
             from: from,
             to: to
         },
         type: "GET",
+        async: true,
         dataTypes: "json"
     });
 
     request.done(function(json) {
-        var tableData = "";
-
-        for (let i = 0; i < json.length; i++) {
-            const item = json[i];
-            
-            var type;
-            if (item["count"] > 100) {
-                type = "custom-badge-green";
-            } else if (item["count"] > 50) {
-                type = "custom-badge-orange";
-            } else {
-                type = "custom-badge-red";
-            }
-
-            var icon;
-            if (item["icon"].length < 3) {
-                icon = "assets/img/missing.png";
-            } else {
-                icon = item["icon"];
-            }
-
-            var name = item["name"];
-            if (item["type"]) name += ", " + item["type"];
-            if (item["links"]) name += " <span class=\"badge custom-badge-gray\">" + item["links"] + " link</span>";
-            if (item["variant"]) name += " <span class=\"badge custom-badge-gray\">" + item["variant"] + "</span>";
-
-            if (item["frameType"] === 4) {
-                if (item["lvl"]) name += " <span class=\"badge custom-badge-gray\">level " + item["lvl"] + "</span>";
-                if (item["quality"]) name += " <span class=\"badge custom-badge-gray\">quality " + item["quality"] + "</span>";
-                if (item["corrupted"]) name += " <span class=\"badge custom-badge-red\">Corrupted</span>";
-            }
-            
-            tableData += "<tr>" +
-            "<td>" + 
-                "<div class=\"table-img-container\"><img src=\"" + icon + "?scale=1&w=1&h=1\"></div>" + name + 
-            "</td>" + 
-            "<td>" + Math.round(item["mean"] * 10) / 10 + "</td>" + 
-            "<td>" + Math.round(item["median"] * 10) / 10 + "</td>" + 
-            "<td>" + Math.round(item["mode"] * 10) / 10 + "</td>" +
-            "<td><span class=\"badge "+type+"\">" + item["count"] + "</span></td>" + 
-            "</tr>";
-        }
-
-        $("#searchResults").append(tableData);
+        // Add downloaded items to global variable ITEMS
+        ITEMS = ITEMS.concat(json);
+        // Parse the downloaded items without parsing already displayed items
+        displayData = parseItems(false);
+        // Add parsed items to the table (table might be cleared in previous function)
+        $("#searchResults").append(displayData);
     });
 }
+
+function parseItems(parseAgain) {
+    // This is the displaystring that will be returned
+    var tableData = "";
+
+    // Loop through all items, parse them and append to displaystring
+    ITEMS.forEach(item => {
+        // Check if the item has already been displayed
+        if ("parsed" in item && item["parsed"] === true) {
+            // Check if the item should be displayed again
+            if (parseAgain) return;
+        }
+        // Mark the item as "parsed"
+        item["parsed"] = true;
+
+        // Format count badge
+        var countBadge;
+        if (item["count"] > 100) countBadge = "<span class=\"badge custom-badge-green\">" + item["count"] + "</span>";
+        else if (item["count"] > 50) countBadge = "<span class=\"badge custom-badge-orange\">" + item["count"] + "</span>";
+        else countBadge = "<span class=\"badge custom-badge-red\">" + item["count"] + "</span>";
+
+        // Format icon
+        var iconDiv = "<div class=\"table-img-container\"><img src=\"" + 
+        (item["icon"] ? item["icon"] : "http://poe.ovh/assets/img/missing.png") + "\"></div>";
+        
+        // Format variant/links badge
+        var name = item["name"];
+        if (item["type"]) name += ", " + item["type"];
+        if (item["links"]) name += " <span class=\"badge custom-badge-gray\">" + item["links"] + " link</span>";
+        if (item["variant"]) name += " <span class=\"badge custom-badge-gray\">" + item["variant"] + "</span>";
+
+        // Add gem badge
+        if (item["frameType"] === 4) {
+            if (item["lvl"]) name += " <span class=\"badge custom-badge-gray\">level " + item["lvl"] + "</span>";
+            if (item["quality"]) name += " <span class=\"badge custom-badge-gray\">quality " + item["quality"] + "</span>";
+            if (item["corrupted"]) name += " <span class=\"badge custom-badge-red\">Corrupted</span>";
+        }
+        
+        // Add it all together
+        tableData += "<tr>" +
+        "<td>" +  iconDiv + name + "</td>" + 
+        "<td>" + roundPrice(item["mean"]) + "</td>" + 
+        "<td>" + roundPrice(item["median"]) + "</td>" + 
+        "<td>" + roundPrice(item["mode"]) + "</td>" +
+        "<td>" + countBadge + "</td>" + 
+        "</tr>";
+    });
+
+    // Return the filled tableData
+    return tableData;
+}
+
 
 function getUrlParameter(sParam) {
     var sPageURL = decodeURIComponent(window.location.search.substring(1)),
@@ -113,3 +130,7 @@ function getUrlParameter(sParam) {
         }
     }
 };
+
+function roundPrice(price) {
+    return Math.round(price * PRICE_PERCISION) / PRICE_PERCISION;
+}
