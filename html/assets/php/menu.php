88d2a9a43e9473a9ec79056826c2a33b5f78a7a7
siegrest <179900@ttu.ee>
2018-03-30 14:29:00 +0300

    Fix undefined variable error

--- html/assets/php/menu.php
+++ html/assets/php/menu.php
@@ -1,18 +1,19 @@
 <?php
-  $jsonFile = json_decode( file_get_contents("assets/json/menu.json"), true );
+$jsonFile = json_decode( file_get_contents("assets/json/menu.json"), true );
 
-  foreach ($jsonFile as $item) {
-    if (strtolower(trim($_GET["category"])) == strtolower(trim($item["title"]))) {
-      $active = " active";
-    } else {
-      $active = "";
-    }
-
-    $row_data = "<a class=\"list-group-item sidebar-link" . $active . "\" href=\"" . $item["href"] . "\" >";
-
-    $row_data .= "<div class=\"sidebar-img-container text-center\">";
-    $row_data .= "<img src=\"" . $item["img"] . "\">";
-    $row_data .= "</div>" . $item["title"] . "</a>";
-    echo $row_data;
+foreach ($jsonFile as $item) {
+  if (empty($_GET["category"])) {
+    $active = "";
+  } else if (strtolower(trim($_GET["category"])) === strtolower(trim($item["title"]))) {
+    $active = " active";
+  } else {
+    $active = "";
   }
-?>
+
+  $row_data = "<a class=\"list-group-item sidebar-link" . $active . "\" href=\"" . $item["href"] . "\" >";
+
+  $row_data .= "<div class=\"sidebar-img-container text-center\">";
+  $row_data .= "<img src=\"" . $item["img"] . "\">";
+  $row_data .= "</div>" . $item["title"] . "</a>";
+  echo $row_data;
+}
