3d5f0e294da06195391384700affc5da12269bff
siegrest <179900@ttu.ee>
2018-03-30 13:22:17 +0300

    Move prices css to separate file

--- html/prices.php
+++ html/prices.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/main.css">
+  <link rel="stylesheet" href="assets/css/prices.css">
   <script src="assets/js/jquery.min.js"></script>
   <script src="assets/js/bootstrap.min.js"></script>
   <script src="assets/js/main.js"></script>
