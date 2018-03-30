a0b7c22f5092162889c689eddcc34b33ab604fad
siegrest <179900@ttu.ee>
2018-03-30 19:55:53 +0300

    Fix headeer spacing

--- html/assets/php/header.php
+++ html/assets/php/header.php
@@ -1,9 +1,9 @@
 <?php
 $title = basename($_SERVER["PHP_SELF"], ".php");
-$index = ($title === "index" ? "active" : "");
-$prices = ($title === "prices" ? "active" : "");
-$about = ($title === "about" ? "active" : "");
-$api = ($title === "api" ? "active" : "");
+$index = ($title === "index" ? " active" : "");
+$prices = ($title === "prices" ? " active" : "");
+$about = ($title === "about" ? " active" : "");
+$api = ($title === "api" ? " active" : "");
 
 echo <<<"HEADER"
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -16,13 +16,12 @@ echo <<<"HEADER"
 
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item"><a class="nav-link $index" href="/">Front</a></li>
-        <li class="nav-item"><a class="nav-link $prices" href="prices">Prices</a></li>
-        <li class="nav-item"><a class="nav-link $api" href="api">API</a></li>
-        <li class="nav-item"><a class="nav-link $about" href="about">About</a></li>
+        <li class="nav-item"><a class="nav-link$index" href="/">Front</a></li>
+        <li class="nav-item"><a class="nav-link$prices" href="prices">Prices</a></li>
+        <li class="nav-item"><a class="nav-link$api" href="api">API</a></li>
+        <li class="nav-item"><a class="nav-link$about" href="about">About</a></li>
       </ul>
     </div>
   </div>
 </nav>
 HEADER;
-?>
