1823b4ba1194f625f061fc812c0582293479b23e
siegrest <179900@ttu.ee>
2018-04-02 11:51:56 +0300

    Switch from conc to heredoc

--- html/assets/php/menu.php
+++ html/assets/php/menu.php
@@ -2,18 +2,18 @@
 $jsonFile = json_decode( file_get_contents("assets/json/menu.json"), true );
 
 foreach ($jsonFile as $item) {
-  if (empty($_GET["category"])) {
-    $active = "";
-  } else if (strtolower(trim($_GET["category"])) === strtolower(trim($item["title"]))) {
-    $active = " active";
-  } else {
-    $active = "";
-  }
+  $category = empty($_GET["category"]) ? "" : strtolower(trim($_GET["category"]));
+  $href = $item["href"];
+  $img = $item["img"];
+  $title = $item["title"];
+  $active = $category === strtolower(trim($title)) ? " active" : "";
 
-  $row_data = "<a class=\"list-group-item sidebar-link" . $active . "\" href=\"" . $item["href"] . "\" >";
-
-  $row_data .= "<div class=\"sidebar-img-container text-center\">";
-  $row_data .= "<img src=\"" . $item["img"] . "\">";
-  $row_data .= "</div>" . $item["title"] . "</a>";
-  echo $row_data;
+  echo <<<"MENU"
+  <a class='list-group-item sidebar-link$active' href='$href'>
+    <div class='sidebar-img-container text-center'>
+      <img src='$img'>
+    </div>
+    $title
+  </a>
+MENU;
 }
