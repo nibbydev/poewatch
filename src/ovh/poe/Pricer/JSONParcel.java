20e05875ab18fb250a39a11e3c3e9a7beb94c29b
siegrest <179900@ttu.ee>
2018-04-11 09:56:44 +0300

    Remove current prices from history

--- src/ovh/poe/Pricer/JSONParcel.java
+++ src/ovh/poe/Pricer/JSONParcel.java
@@ -48,7 +48,7 @@ public class JSONParcel {
                     if (dailyEntry.mean < lowestSpark) lowestSpark = dailyEntry.mean;
                 }
 
-                // Add current mean/median/mode values to history (but not quantity as that's the mean quantity)
+                /*// Add current mean/median/mode values to history (but not quantity as that's the mean quantity)
                 history.mean.add(mean);
                 history.median.add(median);
                 history.mode.add(mode);
@@ -57,7 +57,7 @@ public class JSONParcel {
                 if (history.median.size() > 7) history.median.subList(0, history.median.size() - 7).clear();
                 if (history.mode.size() > 7) history.mode.subList(0, history.mode.size() - 7).clear();
                 // Again, find the lowest mean entry for sparkline
-                if (mean < lowestSpark) lowestSpark = mean;
+                if (mean < lowestSpark) lowestSpark = mean;*/
 
                 // Get the absolute value of lowestSpark as the JS sparkline plugin can't handle negative values
                 if (lowestSpark < 0) lowestSpark *= -1;
