20b053550e001f3ad1240da4d09993d0bf544bd0
siegrest <179900@ttu.ee>
2018-04-29 22:42:18 +0300

    Rename main.js to prices.js

--- html/prices.php
+++ html/prices.php
@@ -245,7 +245,7 @@
 <div class="service-container" id="service-categories" data-payload="<?php echo str_replace('"', "'", file_get_contents( dirname( getcwd(), 2) . "/data/categories.json" ) ); ?>"></div>
 <!--/Service containers/-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script type="text/javascript" src="assets/js/main.js"></script>
+<script type="text/javascript" src="assets/js/prices.js"></script>
 <script type="text/javascript" src="assets/js/sparkline.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
