28d08c9f48114417f8b162f44f79e96fff50aab0
siegrest <179900@ttu.ee>
2018-05-04 10:59:13 +0300

    Add unavailable page

---
+++ html/unavailable.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Poe-Stats - Unavailable</title>
+  <meta charset="utf-8">
+  <link rel="icon" type="image/png" href="assets/img/favico.png">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+<!-- Primary navbar -->
+<nav class="navbar navbar-expand-md navbar-dark">
+  <div class="container-fluid">
+    <a href="/" class="navbar-brand">
+      <img src="assets/img/favico.png" class="d-inline-block align-top mr-2">
+      Poe-Stats
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item"><a class="nav-link" href="/">Front</a></li>
+        <li class="nav-item"><a class="nav-link" href="prices">Prices (not released)</a></li>
+        <li class="nav-item"><a class="nav-link" href="api">API (not released)</a></li>
+        <li class="nav-item"><a class="nav-link" href="progress">Progress</a></li>
+        <li class="nav-item"><a class="nav-link" href="about">About</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!--/Primary navbar/-->
+<!-- Page body -->
+<div class="container-fluid">    
+  <div class="row">
+    <!-- Main content -->
+    <div class="col-10 offset-1 mt-4">
+      <div class="row">
+        <div class="col-lg">
+          <div class="alert custom-card text-center" role="alert">
+            <h3 class="alert-heading">Attention!</h3>
+            <hr>
+            <p>This site is in development!</p>
+            <p>The requested resource has been disabled as it is not ready for release at this point in time.</p>
+          </div>
+        </div>
+      </div> 
+    </div>
+    <!--/Main content/-->
+  </div>
+</div>
+<!--/Page body/-->
+</body>
+</html>
