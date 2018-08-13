a96adc85d45676d5da639c313202edcac6f6f15b
siegrest <179900@ttu.ee>
2018-08-13 22:24:28 +0300

    Fix quality 22 gems indexed separately

--- src/watch/poe/Item.java
+++ src/watch/poe/Item.java
@@ -303,7 +303,7 @@ public class Item extends Mappers.BaseItem {
 
             if (qual < 17) qual = 0;        // quality   0 = 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
             else if (qual < 22) qual = 20;  // quality  20 = 17,18,19,20,21
-                                            // quality  23 = 22,23
+            else qual = 23;                 // quality  23 = 22,23
 
             // Gets rid of specific gems
             if (lvl < 20 && qual > 20) qual = 20;  // lvl:1 quality:23-> lvl:1 quality:20
