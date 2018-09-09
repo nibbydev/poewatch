22c5753c6563e45eb2ae3d59f559b6793943ae0e
siegrest <179900@ttu.ee>
2018-09-09 11:50:51 +0300

    Improve item page chart dataset switching method

--- web/html/item.php
+++ web/html/item.php
@@ -191,7 +191,6 @@
 <!--/Service script/-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
-<script type="text/javascript" src="assets/js/sparkline.js"></script>
 <script type="text/javascript" src="assets/js/item.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
