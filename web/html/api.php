9f95b055ebdba07cda4d789c4f0aa813a0a002c9
siegrest <179900@ttu.ee>
2018-10-04 11:59:13 +0300

    Add cards to api page

--- web/html/api.php
+++ web/html/api.php
@@ -25,37 +25,63 @@
       <!-- Main content -->
       <div class="d-flex w-100 justify-content-center"> 
         <div class='body-boundaries w-100'> 
-
           <div class="row mb-4">
-            <div class="col-lg">
-              <div class="card custom-card">
+            
+            <div class="col-12">
+              <div class="card-deck">
+                <div class="card custom-card mb-3">
+                  <div class="card-header">
+                    <h2 class="m-0"><a href='https://api.poe.watch/leagues' target='_blank'>leagues</a></h2>
+                  </div>
+                  <div class="card-body">
+                    <p class="card-text">List of current leagues. Entries are sorted such that event leagues appear first, followed by the challenge leagues and then the permanent leagues. SSF entries are omitted.</p>
+                  </div>
+                  <div class="card-footer slim-card-edge"></div>
+                </div>
+
+
+                <div class="card custom-card mb-3">
+                  <div class="card-header">
+                    <h2 class="m-0"><a href='https://api.poe.watch/id' target='_blank'>id</a></h2>
+                  </div>
+                  <div class="card-body">
+                    <p class="card-text">Latest change ID from the top of the river and the time it was fetched.</p>
+                  </div>
+                  <div class="card-footer slim-card-edge"></div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-12">
+              <div class="card-deck">
+                <div class="card custom-card mb-3">
+                  <div class="card-header">
+                    <h2 class="m-0"><a href='https://api.poe.watch/categories' target='_blank'>categories</a></h2>
+                  </div>
+                  <div class="card-body">
+                    <p class="card-text">List of categories currently in use.</p>
+                  </div>
+                  <div class="card-footer slim-card-edge"></div>
+                </div>
+
+                <div class="card custom-card mb-3">
+                  <div class="card-header">
+                    <h2 class="m-0"><a href='https://api.poe.watch/itemdata' target='_blank'>itemdata</a></h2>
+                  </div>
+                  <div class="card-body">
+                    <p class="card-text">All items found in the stash API and their defining properties. Category IDs can be found in category API.</p>
+                  </div>
+                  <div class="card-footer slim-card-edge"></div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-12">
+              <div class="card custom-card mb-3">
                 <div class="card-header">
-                  <h2>API</h2>
-                  <div>Use the data in your applications. Rate limiting is 5 requests per 1 second, 1 minute timeout.</div>
+                  <h2 class="m-0"><a href='https://api.poe.watch/get?league=Standard&category=flask' target='_blank'>get</a></h2>
                 </div>
                 <div class="card-body">
-
-                  <h3><a href='https://api.poe.watch/leagues' target='_blank'>leagues</a></h3>
-                  <p class="card-text">List of current leagues. Entries are sorted such that event leagues appear first, followed by the challenge leagues and then the permanent leagues. SSF entries are omitted.</p>
-
-                  <hr>
-                                    
-                  <h3><a href='https://api.poe.watch/id' target='_blank'>id</a></h3>
-                  <p class="card-text">Latest change ID from the top of the river and the time it was fetched.</p>
-
-                  <hr>
-
-                  <h3><a href='https://api.poe.watch/categories' target='_blank'>categories</a></h3>
-                  <p class="card-text">List of categories currently in use.</p>
-
-                  <hr>
-
-                  <h3><a href='https://api.poe.watch/itemdata' target='_blank'>itemdata</a></h3>
-                  <p class="card-text">All items found in the stash API and their defining properties. Category IDs can be found in category API.</p>
-
-                  <hr>
-
-                  <h3><a href='https://api.poe.watch/get?league=Standard&category=flask' target='_blank'>get</a></h3>
                   <p class="card-text">Returns price and item data for specified league and category. Items are listed in decreasing order from most expensive to least expensive. Updated every minute. Capitalization does not matter for request fields.</p>
 
                   <!-- Request fields -->
@@ -85,9 +111,17 @@
                   </div>
                   <!--/Request fields/-->
 
-                  <hr>
+                </div>
+                <div class="card-footer slim-card-edge"></div>
+              </div>
+            </div>
 
-                  <h3><a href='https://api.poe.watch/item?id=259' target='_blank'>item</a></h3>
+            <div class="col-12">
+              <div class="card custom-card mb-3">
+                <div class="card-header">
+                  <h2 class="m-0"><a href='https://api.poe.watch/item?id=259' target='_blank'>item</a></h2>
+                </div>
+                <div class="card-body">
                   <p class="card-text">Retreive detailed information about an item, including its entire price history. Requires an ID, which can be found in itemdata API described above. Currently returns data from all past leagues.</p>
 
                   <!-- Request fields -->
@@ -112,10 +146,18 @@
                   </div>
                   <!--/Request fields/-->
 
-                  <hr>
+                </div>
+                <div class="card-footer slim-card-edge"></div>
+              </div>
+            </div>
 
-                  <h3><a href='https://api.poe.watch/compact?league=Standard' target='_blank'>compact</a> <span class='badge custom-badge-green'>New</span></h3>
-                  <p class="card-text">Return price data (id, mean, median, mode, min, max, count, quantity, exalted) of all items of the provided league. Works only with active leagues. IDs can be found in itemdata API described above.</p>
+            <div class="col-12">
+              <div class="card custom-card mb-3">
+                <div class="card-header">
+                  <h2 class="m-0"><a href='https://api.poe.watch/compact?league=Standard' target='_blank'>compact</a> <span class='badge custom-badge-green'>New</span></h2>
+                </div>
+                <div class="card-body">
+                <p class="card-text">Return price data (id, mean, median, mode, min, max, count, quantity, exalted) of all items of the provided league. Works only with active leagues. IDs can be found in itemdata API described above.</p>
 
                   <!-- Request fields -->
                   <h5 class="card-title">Request fields</h5>
@@ -143,6 +185,7 @@
                 <div class="card-footer slim-card-edge"></div>
               </div>
             </div>
+
           </div>
         </div>
       </div>
