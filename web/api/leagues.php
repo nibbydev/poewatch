fc3a92bf365147589185d3363f53ef09ba8de7e5
siegrest <179900@ttu.ee>
2018-11-11 08:32:11 +0200

    Remove unused function

--- web/api/leagues.php
+++ web/api/leagues.php
@@ -38,55 +38,6 @@ function parse_data($stmt) {
   return $payload;
 }
 
-function parse_history_data($stmt) {
-  $payload = array();
-
-  while ($row = $stmt->fetch()) {
-    // Form a temporary entry array
-    $tmp = array(
-      'league'      => array(
-        'id'        => (int)  $row['leagueId'],
-        'name'      =>        $row['leagueName'],
-        'display'   =>        $row['leagueDisplay'],
-        'active'    => (bool) $row['leagueActive'],
-        'event'     => (bool) $row['leagueEvent'],
-        'start'     =>        $row['leagueStart'],
-        'end'       =>        $row['leagueEnd']
-      ),
-      'mean'      =>          $row['mean']      === NULL ? null : (float) $row['mean'],
-      'median'    =>          $row['median']    === NULL ? null : (float) $row['median'],
-      'mode'      =>          $row['mode']      === NULL ? null : (float) $row['mode'],
-      'exalted'   =>          $row['exalted']   === NULL ? null : (float) $row['exalted'],
-      'count'     =>          $row['count']     === NULL ? null :   (int) $row['count'],
-      'quantity'  =>          $row['quantity']  === NULL ?    0 :   (int) $row['quantity'],
-      'history'   => array()
-    );
-
-    if (!is_null($row['mean_list'])) {
-      // Convert CSVs to arrays
-      $means    = explode(',', $row['mean_list']);
-      $medians  = explode(',', $row['median_list']);
-      $modes    = explode(',', $row['mode_list']);
-      $quants   = explode(',', $row['quantity_list']);
-      $times    = explode(',', $row['time_list']);
-
-      for ($i = 0; $i < sizeof($means); $i++) { 
-        $tmp['history'][] = array(
-          'time'     =>         $times[$i],
-          'mean'     => (float) $means[$i],
-          'median'   => (float) $medians[$i],
-          'mode'     => (float) $modes[$i],
-          'quantity' => (int)   $quants[$i],
-        );
-      }
-    }
-
-    $payload[] = $tmp;
-  }
-
-  return $payload;
-}
-
 // Define content type
 header('Content-Type: application/json');
 
