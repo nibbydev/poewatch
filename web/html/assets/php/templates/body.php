5c996d5d9e48b3b045cc70ab48dbac97b8c48296
siegrest <179900@ttu.ee>
2019-01-09 20:50:48 +0200

    Fix priceNav scaling off to infinity on different browsers

--- web/html/assets/php/templates/body.php
+++ web/html/assets/php/templates/body.php
@@ -8,7 +8,7 @@
           <div class="card-header slim-card-edge"></div>
           <div class="card-body d-flex flex-column p-0">
 <?php global $PAGEDATA; foreach($PAGEDATA["priceCategories"] as $category): ?>
-            <a class="col-12 d-flex align-items-center p-1 pw-hover-light <?php if (isset($_GET["category"]) && $_GET["category"] === $category['name']) echo "pw-highlight " ?>rounded" href="<?php echo $category["href"] ?>" title="<?php echo $category["display"] ?>">
+            <a class="d-flex align-items-center p-1 pw-hover-light <?php if (isset($_GET["category"]) && $_GET["category"] === $category['name']) echo "pw-highlight " ?>rounded" href="<?php echo $category["href"] ?>" title="<?php echo $category["display"] ?>">
               <div class="img-container-sm d-flex justify-content-center align-items-center mr-1">
                 <img class="img-fluid" src="<?php echo $category["icon"] ?>">
               </div>
