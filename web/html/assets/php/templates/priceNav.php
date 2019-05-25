3851f389e5b78896abe45ebac11e7c622dfe339b
siegrest <179900@ttu.ee>
2019-05-25 13:39:26 +0300

    Fix nav highlight without query param

--- web/html/assets/php/templates/priceNav.php
+++ web/html/assets/php/templates/priceNav.php
@@ -1,12 +1,16 @@
-
-<div class="container-fluid second-navbar d-flex d-xl-none align-items-center justify-content-center m-0 p-0"> 
+<div class="container-fluid second-navbar d-flex d-xl-none align-items-center justify-content-center m-0 p-0">
   <div class="row body-boundaries w-100 m-0 px-3">
-<?php foreach($PAGE_DATA["priceCategories"] as $tmp): ?>
-    <a class="col-2 col-md-1 p-0 py-1 d-flex justify-content-center pw-hover-light <?php if (isset($_GET["category"]) && $_GET["category"] === $tmp['name']) echo "pw-highlight " ?>rounded" href="<?php echo $tmp["href"] ?>" title="<?php echo $tmp["display"] ?>">
-      <div class="img-container-sm d-flex justify-content-center align-items-center">
-        <img class="img-fluid" src="<?php echo $tmp["icon"] ?>">
-      </div>
-    </a>
-<?php endforeach; ?>
+    <?php foreach ($PAGE_DATA["priceCategories"] as $tmp) {
+      $highlight = isset($_GET["category"]) && $_GET["category"]
+        || !isset($_GET["category"]) && $tmp['name'] === "currency";
+      ?>
+      <a class="col-2 col-md-1 p-0 py-1 d-flex justify-content-center pw-hover-light rounded
+      <?php if ($highlight) echo "pw-highlight " ?>" href="<?php echo $tmp["href"] ?>"
+         title="<?php echo $tmp["display"] ?>">
+        <div class="img-container-sm d-flex justify-content-center align-items-center">
+          <img class="img-fluid" src="<?php echo $tmp["icon"] ?>" alt="...">
+        </div>
+      </a>
+    <?php } ?>
   </div>
 </div>
