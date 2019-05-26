ca5624251cfa96b003dbca86e41f1b229bd8b507
siegrest <179900@ttu.ee>
2019-05-26 08:46:27 +0300

    Fix default pricenav highlight

--- web/html/assets/php/templates/priceNav.php
+++ web/html/assets/php/templates/priceNav.php
@@ -1,12 +1,11 @@
 <div class="container-fluid second-navbar d-flex d-xl-none align-items-center justify-content-center m-0 p-0">
   <div class="row body-boundaries w-100 m-0 px-3">
     <?php foreach ($PAGE_DATA["priceCategories"] as $tmp) {
-      $highlight = isset($_GET["category"]) && $_GET["category"]
-        || !isset($_GET["category"]) && $tmp['name'] === "currency";
+      $highlight = isset($_GET["category"]) && $_GET["category"] === $tmp['name']
+        || !isset($_GET["category"]) && $tmp['name'] === "currency" && explode(".", basename($_SERVER['PHP_SELF']))[0] === "prices";
       ?>
-      <a class="col-2 col-md-1 p-0 py-1 d-flex justify-content-center pw-hover-light rounded
-      <?php if ($highlight) echo "pw-highlight " ?>" href="<?php echo $tmp["href"] ?>"
-         title="<?php echo $tmp["display"] ?>">
+      <a class="col-2 col-md-1 p-0 py-1 d-flex justify-content-center pw-hover-light rounded <?php if ($highlight) echo "pw-highlight" ?>"
+         href="<?php echo $tmp["href"] ?>" title="<?php echo $tmp["display"] ?>">
         <div class="img-container-sm d-flex justify-content-center align-items-center">
           <img class="img-fluid" src="<?php echo $tmp["icon"] ?>" alt="...">
         </div>
